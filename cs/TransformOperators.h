c6966f17cc3f51672307cc35b2d059b030a31b74
Carlo Wood <carlo@alinoe.com>
2026-02-07 22:09:44 +0100

    Move operator*(Rectangle, Transform) back to cairowindow.

--- cs/TransformOperators.h
+++ cs/TransformOperators.h
@@ -39,18 +39,4 @@ Size<to_cs> operator*(Size<from_cs> const& size, math::Transform<from_cs, to_cs,
   }
 }
 
-#if 0 //FIXME: move this to cairowindow
-template<CS from_cs, CS to_cs, bool inverted, math::AffineTransformConcept AffineTransformBackend>
-Rectangle<to_cs> operator*(Rectangle<from_cs> const& rectangle, math::Transform<from_cs, to_cs, inverted, AffineTransformBackend> const& transform)
-{
-  Point<from_cs> p_from_cs{rectangle.offset_x(), rectangle.offset_y()};
-  Size<from_cs> s_from_cs{rectangle.width(), rectangle.height()};
-
-  Point<to_cs> p_to_cs = p_from_cs * transform;
-  Size<to_cs> s_to_cs = s_from_cs * transform;
-
-  return {p_to_cs, s_to_cs};
-}
-#endif
-
 } // namespace math::cs
